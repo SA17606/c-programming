aa37c6517b00242db61da2eb54f79eb2d90a2bc0
SA17606 <122534607+SA17606@users.noreply.github.com>
2023-02-08 02:02:54 +0300

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,14 @@
+//program to check a persons eligibility to vote
+#include <stdio.h>
+int main()
+{
+  int age, birth,kenya;
+  printf ("enter age in years and birth:");
+  scanf ("%d%d",&age,&birth);
+  if (age>=18, birth=kenya){
+    printf ("eligible to vote");} 
+  else
+  {printf ("not eligible to vote");}
+  return 0;
+}
+  
