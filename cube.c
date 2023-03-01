69f494da424c71d05eba0897a9a9ddcaa257b302
SA17606 <122534607+SA17606@users.noreply.github.com>
2023-03-01 23:37:21 +0300

    Add files via upload

---
+++ cube.c
@@ -0,0 +1,12 @@
+//natural number
+#include <stdio.h>
+int main() {
+    int i,cube 1;
+for (i=1,i<=5,i++)
+  {
+  printf("%d ",i);
+  }
+  cube=i^3;
+printf("for loop body finished");
+return 0;
+  }
