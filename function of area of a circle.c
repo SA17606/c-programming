b9a4336fe916d4327419a6db0c488f60f43ca07d
SA17606 <122534607+SA17606@users.noreply.github.com>
2023-03-02 15:23:27 +0300

    Create function of area of a circle.c

---
+++ function of area of a circle.c
@@ -0,0 +1,23 @@
+//fuction of area of a circle
+#include<stdio.h>
+#define pi 3.142
+int area(int r);
+int main()
+{
+    int r,result;
+    printf("\nEnter the radius:");
+    scanf("%d",&r);
+    
+    result=pi*r*r;
+    printf("\nThe area of a circle is: %d",result);
+    
+    return 0;
+}
+
+int area(int r)
+{
+    int answer;
+    answer= pi*r*r;
+    
+    return answer;
+}
