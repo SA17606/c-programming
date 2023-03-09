d4dfee5aee5d1ed91e1bb2a9027842358125bd05
SA17606 <122534607+SA17606@users.noreply.github.com>
2023-03-09 15:26:34 +0300

    Add files via upload

---
+++ array marks.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+    int marks [10]={22,22,30,40,19,12,32,40,28,25};
+  for(i=0;i<10;i++)
+    {
+      printf("%d ",marks[i]);
+    }
+    return 0;
+}
+  
